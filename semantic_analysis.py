04e6636c1f0a5b13963fbc7ce6fca3d21750215f
Your Name <kinshat1995@gmail.com>
2019-12-18 00:52:48 +0100

    semantic analysis of text

---
+++ semantic_analysis.py
@@ -0,0 +1,8 @@
+from nltk.corpus import wordnet
+
+wOne = wordnet.synset("program.n.01");
+wThree = wordnet.synset("execute.n.01")
+wTwo = wordnet.synset("university.n.01")
+
+
+print(wOne.wup_similarity(wThree))
