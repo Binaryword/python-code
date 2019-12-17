573029dad9718b2b5753d50ee5f8b701ce71bcba
Your Name <kinshat1995@gmail.com>
2019-12-17 23:18:21 +0100

    nltk stop word demo

---
+++ stopword_demo.py
@@ -0,0 +1,13 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+sample_word = "this is the coolest word ever and it is ok for me"
+
+
+token_word = word_tokenize(sample_word)
+stopWord = set(stopwords.words("english"))
+print(token_word)
+
+processedWord = [st for st in token_word if st not in stopWord]
+print(processedWord)
+
