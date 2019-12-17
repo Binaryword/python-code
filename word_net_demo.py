5dcbb5cf6679c75670fe54924948ce9ed004d9e4
Your Name <kinshat1995@gmail.com>
2019-12-18 00:18:05 +0100

    printing word net defination and example

---
+++ word_net_demo.py
@@ -0,0 +1,16 @@
+from nltk.corpus import wordnet
+
+synset = wordnet.synsets("program")
+print(synset)
+print("lenght of sysnset " , len(synset))
+print(synset[0].definition())
+
+print("\n\n\n\t printing defination associated to each word\n\n")
+
+for word in synset:
+    print(word.name() , word.definition())
+    print("example: " , word.examples())
+
+
+
+    
