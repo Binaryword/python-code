b66c130ae85e0f0236752119b9c6fe89b243b86d
Your Name <kinshat1995@gmail.com>
2020-01-06 04:27:44 +0100

    word part of speach tag function

---
+++ Precision Code/part of speach tag.py
@@ -0,0 +1,40 @@
+from nltk.corpus import wordnet
+
+syn = wordnet.synsets("dog")
+
+nLenght = 0
+vLenght = 0
+oLenght = 0
+
+
+print(f"total sences is {len(syn)}")
+
+def getPOS(synsetwords):
+    ## adding verb and now empty list with
+    ## with other parth of speach tagin..   
+    vList = []
+    nList = []
+    oList = []
+
+    posList = [pos.pos() for pos in synsetwords]
+    for pl in posList:
+        if pl == "v":
+            vList.append(pl)
+        elif pl == "n":
+            nList.append(pl)
+        else:
+            oList.append(pl)
+
+    pos_data = [("noun" , len(nList)),
+    ("verb" , len(vList)),
+    ("others" , len(oList)) ]
+    return pos_data
+
+print(getPOS(syn))
+
+for w in syn:
+    print(w.name())
+    for l in w.lemmas():
+        print(l.name())
+
+    print("\n")
