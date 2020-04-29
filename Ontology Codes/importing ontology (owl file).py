4eb7f785999b86d0417e36b5788e0b4e87d9325e
Your Name <kinshat1995@gmail.com>
2020-04-29 10:14:03 +0100

    import OWL file and print class & object property

---
+++ Ontology Codes/importing ontology (owl file).py
@@ -0,0 +1,25 @@
+from owlready2 import*
+
+#ont = get_ontology("C:\\Users\\binary\\Videos\\MY PYTHON CLASS\\PythonCode\\Ontology Codes\\newMaizeOntology.owl")
+ont = get_ontology("newMaizeOntology.owl")
+ont.load()
+print(ont.Climate)
+ont_class = ont.classes()
+ont_Oproperty = ont.object_properties()
+##print(list(ont_class))
+
+i = 0
+
+print("THE CLASSES IN THE ONTOLOGY)
+for cla in list(ont_class):
+    i=i+1
+    print(f"{i} >> {cla}")
+
+
+print("THE OBJECT PROPERTY IN THE ONTOLOGY")
+for ob in list(ont_Oproperty):
+    i=i+1
+    print(f"{i} >> {ob}")
+    
+
+
